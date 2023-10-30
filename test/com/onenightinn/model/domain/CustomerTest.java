0777160bde199e2b9bbdff6cd64a955ad2fc1e5e
alvaral10 <alvaral10@gmail.com>
2023-10-30 09:50:10 -0600

    Updated folders

---
+++ test/com/onenightinn/model/domain/CustomerTest.java
@@ -0,0 +1,26 @@
+package com.onenightinn.model.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CustomerTest {
+    private Customer customer1;
+
+    @BeforeEach
+    void setUp() {
+        /*
+         Create a new Customer object before each test
+         */
+        customer1 = new Customer("Kitty", "Hello", "hk@sanio.com", "password", "1234567890", "9876543210", null, null);
+    }
+
+    @Test
+    void testValidateValidCustomer() {
+        System.out.println("starting testValidate Customer()");
+        assertTrue(customer1.validate());
+        System.out.println("testValidate PASSED");
+    }
+
+}
