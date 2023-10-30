5dfebc326dc77e70192994eeb440ab11ac219f58
alvaral10 <alvaral10@gmail.com>
2023-10-30 06:31:48 -0600

    Updated folders

--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -1,5 +1,4 @@
-package com.onenightinn.model.domain;
-
+import com.onenightinn.model.domain.Customer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
