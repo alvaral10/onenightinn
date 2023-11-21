21c52e604d0e3d02148297dac238f4829bc7e263
alvaral10 <alvaral10@gmail.com>
2023-11-20 18:59:23 -0700

    Week 5 Commits

--- test/com/onenightinn/model/services/factory/ServiceFactoryTest.java
+++ test/com/onenightinn/model/services/factory/ServiceFactoryTest.java
@@ -3,39 +3,29 @@ package  com.onenightinn.model.services.factory;
 import com.onenightinn.model.business.exception.ServiceLoadException;
 import com.onenightinn.model.services.loginservice.ILoginService;
 import com.onenightinn.model.services.loginservice.LoginServiceImpl;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
 
-public class ServiceFactoryTest {
+public class ServiceFactoryTest extends TestCase {
 
 
     ServiceFactory serviceFactory;
 
-    @BeforeEach
     public void setUp() throws Exception {
         serviceFactory = ServiceFactory.getInstance();
     }
 
-
-    /**
-     * Test Factory to return the login service and assert it by
-     * checking it is an instance of LoginServiceImpl
-     * This should be true since LoginServiceImpl implements ILoginService
-     */
-    @Test
+    public void testGetInstance(){
+        assertNotNull(serviceFactory);
+    }
     public void testGetLoginService() {
-        ILoginService loginService;
+
         try {
-            loginService = (ILoginService) serviceFactory.getService(ILoginService.NAME);
-            Assertions.assertTrue(loginService instanceof LoginServiceImpl);
+            ILoginService loginService = (ILoginService)serviceFactory.getService(ILoginService.NAME);
+            assertTrue(loginService instanceof LoginServiceImpl);
             System.out.println("testGetLoginService PASSED");
         } catch (ServiceLoadException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
-            Assertions.fail("ServiceLoadException");
         }
-
     }
 
 }
